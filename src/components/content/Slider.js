5e496a157f760c69bdccfb35bf7a3aebb84d4f05
Maksym Haltsev <maksym.haltsev@gmail.com>
2019-04-29 14:28:07 +0300

    fix slider

--- src/components/content/Slider.js
+++ src/components/content/Slider.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { Carousel } from 'react-bootstrap'
+import { Carousel } from 'react-bootstrap';
+import sirSlide1 from '../../assets/img/sir-slide-1.jpg';
+import sirSlide2 from '../../assets/img/sir-slide-2.jpg';
+import sirSlide3 from '../../assets/img/sir-slide-3.jpg';
+
+const textStyle = {
+    color: 'white',
+    textShadow: '0 0 5px rgba(0, 0, 0, 0.5)'
+}
 
 function Slider() {
   return (
@@ -8,35 +16,35 @@ function Slider() {
             <Carousel.Item>
                 <img
                 className="d-block w-100"
-                src="https://cs6.pikabu.ru/post_img/big/2014/08/07/2/1407372313_1777995650.jpg"
+                src={sirSlide1}
                 alt="First slide"
                 />
                 <Carousel.Caption>
-                <h3 style={{color:'white'}}>Сударь лучше!</h3>
-                <p style={{color:'white'}}>Сударь, это язык с низким порогом вхождения, без пугающих неочевидных синтаксических знаков, которые распространены в других языках</p>
+                <h3 style={textStyle}>Сударь лучше!</h3>
+                <p style={textStyle}>Сударь, это язык с низким порогом вхождения, без пугающих неочевидных синтаксических знаков, которые распространены в других языках</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
                 className="d-block w-100"
-                src="https://www.meme-arsenal.com/memes/76c8c80c9ef825020a820b2171559aaf.jpg"
+                src={sirSlide2}
                 alt="Third slide"
                 />
                 <Carousel.Caption>
-                <h3>Сударь, это просто!</h3>
-                <p>Попробуй и ты поймешь!</p>
+                <h3 style={textStyle}>Сударь, это просто!</h3>
+                <p style={textStyle}>Попробуй и ты поймешь!</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
                 className="d-block w-100"
-                src="https://besplatka.ua/aws/42/03/56/93/programmist-srochno-photo-fd7e.jpg"
+                src={sirSlide3}
                 alt="Third slide"
                 />
 
                 <Carousel.Caption>
-                <h3 style={{color:'white'}}>Начни прямо сейчас</h3>
-                <p style={{color:'white'}}>Попробуй запустить свою первую программу на языке программирования Сударь!</p>
+                <h3 style={textStyle}>Начни прямо сейчас</h3>
+                <p style={textStyle}>Попробуй запустить свою первую программу на языке программирования Сударь!</p>
                 </Carousel.Caption>
             </Carousel.Item>
             </Carousel>
