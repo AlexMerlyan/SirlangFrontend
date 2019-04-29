24761e4299f1cbfd353f9e4286e759573d19c182
Maksym Haltsev <maksym.haltsev@gmail.com>
2019-04-29 14:29:04 +0300

    fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import imgUrl from '../assets/img/footerImg.jpg';
+import imgUrl from '../assets/img/i-dont-know.jpg';
 
 var style = {
     backgroundColor: "#F8F8F8",
@@ -25,8 +25,11 @@ function Footer() {
         <div>
             <div style={phantom} />
             <div style={style}>
-            <p><img src={imgUrl} alt="" />Все права защищены, а может быть и нет<img  src={imgUrl} alt="" /></p>
-            <img src={imgUrl} alt="" />
+            <p>
+                <img src={imgUrl} alt="" />
+                <span style={{ margin: '0 10px' }}>Все права защищены, а может быть и нет</span>
+                <img  src={imgUrl} alt="" />
+            </p>
             </div>
         </div>
     )
